a5f4cdd732e72b2f2d2d1b6bdfe7d33d7f8e1612
root <rodrigo@taller.net.br>
2014-07-26 13:34:19 -0300

    Added drugen skeleton.

---
+++ src/Form/DrugenForm.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drugen\Form\DrugenForm.
+ */
+
+namespace \Drupal\drugen\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+
+class DrugenForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getFormId() {
+    return 'drugen_config_form';
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function buildForm(array $form, array &$form_state) {
+    // $m4032404_admin_only = \Drupal::config('m4032404.settings')
+    //   ->get('m4032404_admin_only', FALSE);
+
+    $form['Teste'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Enforce on Admin Only'),
+      '#description' => t('Check the box to enforce the 404 behavior only on admin paths'),
+      //'#default_value' => $m4032404_admin_only,
+    );
+
+    // $form['submit'] = array(
+    //   '#type' => 'submit',
+    //   '#value' => $this->t('Save configuration'),
+    // );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    // \Drupal::config('m4032404.settings')
+    //   ->set('m4032404_admin_only', $form_state['values']['m4032404_admin_only'])
+    //   ->save();
+    // parent::submitForm($form, $form_state);
+  }
+}
