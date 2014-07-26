ab9ee79dc0d5498c22ae3d7cd7a4abef35b2bb42
root <rodrigo@taller.net.br>
2014-07-26 15:30:28 -0300

    Added fields and submit button.

--- src/Form/DrugenForm.php
+++ src/Form/DrugenForm.php
@@ -2,61 +2,69 @@
 
 /**
  * @file
- * Contains \Drupal\m4032404\Form\m4032404Form.
+ * Contains \Drupal\drugen\Form\DrugenForm.
  */
 
 namespace Drupal\drugen\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 
-class DrugenForm extends ConfigFormBase {
+class DrugenForm extends ConfigFormBase
+{
 
-  /**
-   * {@inheritdoc}.
-   */
-  public function getFormId() {
-    return 'drugen_config_form';
-  }
+    /**
+     * {@inheritdoc}.
+     */
+    public function getFormId()
+    {
+        return 'drugen_config_form';
+    }
 
-  /**
-   * {@inheritdoc}.
-   */
-  public function buildForm(array $form, array &$form_state) {
-    // $m4032404_admin_only = \Drupal::config('m4032404.settings')
-    //   ->get('m4032404_admin_only', FALSE);
+    /**
+     * {@inheritdoc}.
+     */
+    public function buildForm(array $form, array &$form_state)
+    {
+        $form['drugen_user_quantity'] = array(
+            '#type' => 'textfield',
+            '#title' => t('User quantity'),
+            '#description' => t('Type the number of users Drugen will generate.'),
+            '#required' => true,
+        );
 
-    $form['Teste'] = array(
-      '#type' => 'checkbox',
-      '#title' => t('Enforce on Admin Only'),
-      '#description' => t('Check the box to enforce the 404 behavior only on admin paths'),
-      //'#default_value' => $m4032404_admin_only,
-    );
+        $form['drugen_username_prefix'] = array(
+            '#type' => 'textfield',
+            '#title' => t('User name prefix'),
+            '#description' => t('Type the prefix for user name (optional).'),
+        );
+        
+         $form['submit'] = array(
+             '#type' => 'submit',
+             '#value' => $this->t('Generate users'),
+         );
 
-    // $form['submit'] = array(
-    //   '#type' => 'submit',
-    //   '#value' => $this->t('Save configuration'),
-    // );
+        return $form;
+    }
 
-    return $form;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function validateForm(array &$form, array &$form_state)
+    {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, array &$form_state) {
+        /*if (strpos($form_state['values']['email'], '.com') === FALSE ) {
+            $this->setFormError('email', $form_state, $this->t('This is not a .com email address.'));
+        } */
+    }
 
-    /*if (strpos($form_state['values']['email'], '.com') === FALSE ) {
-      $this->setFormError('email', $form_state, $this->t('This is not a .com email address.'));
-    } */
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, array &$form_state) {
-    // \Drupal::config('m4032404.settings')
-    //   ->set('m4032404_admin_only', $form_state['values']['m4032404_admin_only'])
-    //   ->save();
-    // parent::submitForm($form, $form_state);
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, array &$form_state)
+    {
+        // \Drupal::config('m4032404.settings')
+        //     ->set('m4032404_admin_only', $form_state['values']['m4032404_admin_only'])
+        //     ->save();
+        // parent::submitForm($form, $form_state);
+    }
 }
